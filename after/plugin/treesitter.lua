2604715f7c8aacfa276ce1c464c7777b47fa114a
maziogra <mattiagravellu@gmail.com>
2024-03-31 18:12:43 +0200

    disable html auto-installation

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,12 +2,16 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = {"javascript", "typescript", "css", "java", "c", "lua" },
 
+  
+
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
+
+  ignore_install = { "html" },
 
   highlight = {
     enable = true,
