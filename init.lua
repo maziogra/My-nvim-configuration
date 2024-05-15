b8ff3f9587b3e283561d465eab079add2f16d8df
maziogra <mattiagravellu@gmail.com>
2024-05-15 22:15:01 +0200

    add autocompletion

--- init.lua
+++ init.lua
@@ -1,13 +1,3 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.opt.guicursor = "n-v-i-c:block-Cursor"
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -20,5 +10,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
+require("vim-options")
 require("lazy").setup("plugins")
---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
